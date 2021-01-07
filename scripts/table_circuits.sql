c6046f5d876e2d95c68a619711e9cdc0f05ae932
Alexandru Ica <alexandruica703@gmail.com>
2021-01-07 12:21:09 +0200

    Forgot to add `circuits` table

---
+++ scripts/table_circuits.sql
@@ -0,0 +1,154 @@
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(1, 'Albert Park Grand Prix Circuit', 'Melbourne', 'Australia', 'http://en.wikipedia.org/wiki/Melbourne_Grand_Prix_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(2, 'Sepang International Circuit', 'Kuala Lumpur', 'Malaysia', 'http://en.wikipedia.org/wiki/Sepang_International_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(3, 'Bahrain International Circuit', 'Sakhir', 'Bahrain', 'http://en.wikipedia.org/wiki/Bahrain_International_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(4, 'Circuit de Barcelona-Catalunya', 'Montmeló', 'Spain', 'http://en.wikipedia.org/wiki/Circuit_de_Barcelona-Catalunya');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(5, 'Istanbul Park', 'Istanbul', 'Turkey', 'http://en.wikipedia.org/wiki/Istanbul_Park');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(6, 'Circuit de Monaco', 'Monte-Carlo', 'Monaco', 'http://en.wikipedia.org/wiki/Circuit_de_Monaco');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(7, 'Circuit Gilles Villeneuve', 'Montreal', 'Canada', 'http://en.wikipedia.org/wiki/Circuit_Gilles_Villeneuve');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(8, 'Circuit de Nevers Magny-Cours', 'Magny Cours', 'France', 'http://en.wikipedia.org/wiki/Circuit_de_Nevers_Magny-Cours');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(9, 'Silverstone Circuit', 'Silverstone', 'UK', 'http://en.wikipedia.org/wiki/Silverstone_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(10, 'Hockenheimring', 'Hockenheim', 'Germany', 'http://en.wikipedia.org/wiki/Hockenheimring');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(11, 'Hungaroring', 'Budapest', 'Hungary', 'http://en.wikipedia.org/wiki/Hungaroring');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(12, 'Valencia Street Circuit', 'Valencia', 'Spain', 'http://en.wikipedia.org/wiki/Valencia_Street_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(13, 'Circuit de Spa-Francorchamps', 'Spa', 'Belgium', 'http://en.wikipedia.org/wiki/Circuit_de_Spa-Francorchamps');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(14, 'Autodromo Nazionale di Monza', 'Monza', 'Italy', 'http://en.wikipedia.org/wiki/Autodromo_Nazionale_Monza');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(15, 'Marina Bay Street Circuit', 'Marina Bay', 'Singapore', 'http://en.wikipedia.org/wiki/Marina_Bay_Street_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(16, 'Fuji Speedway', 'Oyama', 'Japan', 'http://en.wikipedia.org/wiki/Fuji_Speedway');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(17, 'Shanghai International Circuit', 'Shanghai', 'China', 'http://en.wikipedia.org/wiki/Shanghai_International_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(18, 'Autódromo José Carlos Pace', 'São Paulo', 'Brazil', 'http://en.wikipedia.org/wiki/Aut%C3%B3dromo_Jos%C3%A9_Carlos_Pace');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(19, 'Indianapolis Motor Speedway', 'Indianapolis', 'USA', 'http://en.wikipedia.org/wiki/Indianapolis_Motor_Speedway');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(20, 'Nürburgring', 'Nürburg', 'Germany', 'http://en.wikipedia.org/wiki/N%C3%BCrburgring');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(21, 'Autodromo Enzo e Dino Ferrari', 'Imola', 'Italy', 'http://en.wikipedia.org/wiki/Autodromo_Enzo_e_Dino_Ferrari');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(22, 'Suzuka Circuit', 'Suzuka', 'Japan', 'http://en.wikipedia.org/wiki/Suzuka_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(23, 'A1-Ring', 'Spielburg', 'Austria', 'http://en.wikipedia.org/wiki/A1-Ring');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(24, 'Yas Marina Circuit', 'Abu Dhabi', 'UAE', 'http://en.wikipedia.org/wiki/Yas_Marina_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(25, 'Autódromo Juan y Oscar Gálvez', 'Buenos Aires', 'Argentina', 'http://en.wikipedia.org/wiki/Aut%C3%B3dromo_Oscar_Alfredo_G%C3%A1lvez');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(26, 'Circuito de Jerez', 'Jerez de la Frontera', 'Spain', 'http://en.wikipedia.org/wiki/Circuito_Permanente_de_Jerez');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(27, 'Autódromo do Estoril', 'Estoril', 'Portugal', 'http://en.wikipedia.org/wiki/Aut%C3%B3dromo_do_Estoril');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(28, 'Okayama International Circuit', 'Okayama', 'Japan', 'http://en.wikipedia.org/wiki/TI_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(29, 'Adelaide Street Circuit', 'Adelaide', 'Australia', 'http://en.wikipedia.org/wiki/Adelaide_Street_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(30, 'Kyalami', 'Midrand', 'South Africa', 'http://en.wikipedia.org/wiki/Kyalami');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(31, 'Donington Park', 'Castle Donington', 'UK', 'http://en.wikipedia.org/wiki/Donington_Park');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(32, 'Autódromo Hermanos Rodríguez', 'Mexico City', 'Mexico', 'http://en.wikipedia.org/wiki/Aut%C3%B3dromo_Hermanos_Rodr%C3%ADguez');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(33, 'Phoenix street circuit', 'Phoenix', 'USA', 'http://en.wikipedia.org/wiki/Phoenix_street_circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(34, 'Circuit Paul Ricard', 'Le Castellet', 'France', 'http://en.wikipedia.org/wiki/Paul_Ricard_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(35, 'Korean International Circuit', 'Yeongam County', 'Korea', 'http://en.wikipedia.org/wiki/Korean_International_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(36, 'Autódromo Internacional Nelson Piquet', 'Rio de Janeiro', 'Brazil', 'http://en.wikipedia.org/wiki/Aut%C3%B3dromo_Internacional_Nelson_Piquet');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(37, 'Detroit Street Circuit', 'Detroit', 'USA', 'http://en.wikipedia.org/wiki/Detroit_street_circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(38, 'Brands Hatch', 'Kent', 'UK', 'http://en.wikipedia.org/wiki/Brands_Hatch');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(39, 'Circuit Park Zandvoort', 'Zandvoort', 'Netherlands', 'http://en.wikipedia.org/wiki/Circuit_Zandvoort');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(40, 'Zolder', 'Heusden-Zolder', 'Belgium', 'http://en.wikipedia.org/wiki/Zolder');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(41, 'Dijon-Prenois', 'Dijon', 'France', 'http://en.wikipedia.org/wiki/Dijon-Prenois');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(42, 'Fair Park', 'Dallas', 'USA', 'http://en.wikipedia.org/wiki/Fair_Park');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(43, 'Long Beach', 'California', 'USA', 'http://en.wikipedia.org/wiki/Long_Beach,_California');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(44, 'Las Vegas Street Circuit', 'Nevada', 'USA', 'http://en.wikipedia.org/wiki/Las_Vegas,_Nevada');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(45, 'Jarama', 'Madrid', 'Spain', 'http://en.wikipedia.org/wiki/Circuito_Permanente_Del_Jarama');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(46, 'Watkins Glen', 'New York State', 'USA', 'http://en.wikipedia.org/wiki/Watkins_Glen_International');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(47, 'Scandinavian Raceway', 'Anderstorp', 'Sweden', 'http://en.wikipedia.org/wiki/Scandinavian_Raceway');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(48, 'Mosport International Raceway', 'Ontario', 'Canada', 'http://en.wikipedia.org/wiki/Mosport');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(49, 'Montjuïc', 'Barcelona', 'Spain', 'http://en.wikipedia.org/wiki/Montju%C3%AFc_circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(50, 'Nivelles-Baulers', 'Brussels', 'Belgium', 'http://en.wikipedia.org/wiki/Nivelles-Baulers');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(51, 'Charade Circuit', 'Clermont-Ferrand', 'France', 'http://en.wikipedia.org/wiki/Charade_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(52, 'Circuit Mont-Tremblant', 'Quebec', 'Canada', 'http://en.wikipedia.org/wiki/Circuit_Mont-Tremblant');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(53, 'Rouen-Les-Essarts', 'Rouen', 'France', 'http://en.wikipedia.org/wiki/Rouen-Les-Essarts');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(54, 'Le Mans', 'Le Mans', 'France', 'http://en.wikipedia.org/wiki/Circuit_de_la_Sarthe#Bugatti_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(55, 'Reims-Gueux', 'Reims', 'France', 'http://en.wikipedia.org/wiki/Reims-Gueux');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(56, 'Prince George Circuit', 'Eastern Cape Province', 'South Africa', 'http://en.wikipedia.org/wiki/Prince_George_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(57, 'Zeltweg', 'Styria', 'Austria', 'http://en.wikipedia.org/wiki/Zeltweg_Airfield');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(58, 'Aintree', 'Liverpool', 'UK', 'http://en.wikipedia.org/wiki/Aintree_Motor_Racing_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(59, 'Circuito da Boavista', 'Oporto', 'Portugal', 'http://en.wikipedia.org/wiki/Circuito_da_Boavista');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(60, 'Riverside International Raceway', 'California', 'USA', 'http://en.wikipedia.org/wiki/Riverside_International_Raceway');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(61, 'AVUS', 'Berlin', 'Germany', 'http://en.wikipedia.org/wiki/AVUS');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(62, 'Monsanto Park Circuit', 'Lisbon', 'Portugal', 'http://en.wikipedia.org/wiki/Monsanto_Park_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(63, 'Sebring International Raceway', 'Florida', 'USA', 'http://en.wikipedia.org/wiki/Sebring_Raceway');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(64, 'Ain Diab', 'Casablanca', 'Morocco', 'http://en.wikipedia.org/wiki/Ain-Diab_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(65, 'Pescara Circuit', 'Pescara', 'Italy', 'http://en.wikipedia.org/wiki/Pescara_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(66, 'Circuit Bremgarten', 'Bern', 'Switzerland', 'http://en.wikipedia.org/wiki/Circuit_Bremgarten');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(67, 'Circuit de Pedralbes', 'Barcelona', 'Spain', 'http://en.wikipedia.org/wiki/Pedralbes_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(68, 'Buddh International Circuit', 'Uttar Pradesh', 'India', 'http://en.wikipedia.org/wiki/Buddh_International_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(69, 'Circuit of the Americas', 'Austin', 'USA', 'http://en.wikipedia.org/wiki/Circuit_of_the_Americas');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(70, 'Red Bull Ring', 'Spielburg', 'Austria', 'http://en.wikipedia.org/wiki/Red_Bull_Ring');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(71, 'Sochi Autodrom', 'Sochi', 'Russia', 'http://en.wikipedia.org/wiki/Sochi_Autodrom');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(72, 'Port Imperial Street Circuit', 'New Jersey', 'USA', 'http://en.wikipedia.org/wiki/Port_Imperial_Street_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(73, 'Baku City Circuit', 'Baku', 'Azerbaijan', 'http://en.wikipedia.org/wiki/Baku_City_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(74, 'Hanoi Street Circuit', 'Hanoi', 'Vietnam', 'http://en.wikipedia.org/wiki/Hanoi_Street_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(75, 'Autódromo Internacional do Algarve', 'Portimão', 'Portugal', 'http://en.wikipedia.org/wiki/Algarve_International_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(76, 'Autodromo Internazionale del Mugello', 'Mugello', 'Italy', 'http://en.wikipedia.org/wiki/Mugello_Circuit');
+INSERT INTO circuits(circuit_id, name, location, country, url)
+	VALUES(77, 'Jeddah Street Circuit', 'Jeddah', 'Saudi Arabia', 'http://en.wikipedia.org/wiki/Jeddah_Street_Circuit');
